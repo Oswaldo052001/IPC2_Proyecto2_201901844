7802816ccef613eb79137528883b26fa7461230d
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-09-27 18:55:10 -0600

    Se realizó la función para crear el archivo de salida de los mensajes procesados

---
+++ Movimientos.py
@@ -0,0 +1,17 @@
+
+class Movimientos():
+
+    def __init__(self,tiempo,movimiento):
+        self.tiempo = tiempo
+        self.movimiento = movimiento
+
+    def getTiempo(self):
+        return self.tiempo
+    
+    def getMovimiento(self):
+        return self.movimiento
+    
+    
+
+    
+
